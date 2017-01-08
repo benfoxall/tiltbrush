f2476296fbe68d6797cc9ae975dd5feb44f0d43e
Ben Foxall <benfoxall@gmail.com>
2017-01-08 09:13:50 +0000

    better variable naming

--- docs/tiltbrush.js
+++ docs/tiltbrush.js
@@ -4,7 +4,7 @@
 const text = (int32) =>
   [0, 8, 16, 24].map(
     d => String.fromCharCode(0xff & (int32 >>> d))
-  ).join('')
+  ).join('');
 
 
 // https://gist.github.com/Plutor/2002457
@@ -12,72 +12,72 @@ const text = (int32) =>
 
 
 
-const values = Array.from({length: 32}, (_,x) => Math.pow(2, x))
+const values = Array.from({length: 32}, (_,x) => Math.pow(2, x));
 
 const bits = i =>
-  values.map( d => !! (i & d) )
+  values.map( d => !! (i & d) );
 
 const range = n =>
-  Array.from({length: n}, (_, i) => i)
+  Array.from({length: n}, (_, i) => i);
 
 
 class Reader {
 
   constructor (view, endian) {
-    this.view = view
-    this.i = 0
-    this.endian = endian
+    this.view = view;
+    this.cursor = 0;
+    this.endian = endian;
   }
 
   skip (i) {
-    this.i += i
+    this.cursor += i;
   }
 
   getInt8() {
-    const value = this.view.getInt8(this.i, this.endian)
-    this.i += 1
+    const value = this.view.getInt8(this.cursor, this.endian);
+    this.cursor += 1;
     return value
   }
 
   getUint8() {
-    const value = this.view.getUint8(this.i, this.endian)
-    this.i += 1
+    const value = this.view.getUint8(this.cursor, this.endian);
+    this.cursor += 1;
     return value
   }
 
   getInt16() {
-    const value = this.view.getInt16(this.i, this.endian)
-    this.i += 2
+    const value = this.view.getInt16(this.cursor, this.endian);
+    this.cursor += 2;
     return value
   }
 
   getUint16() {
-    const value = this.view.getUint16(this.i, this.endian)
-    this.i += 2
+    const value = this.view.getUint16(this.cursor, this.endian);
+    this.cursor += 2;
     return value
   }
 
   getInt32() {
-    const value = this.view.getInt32(this.i, this.endian)
-    this.i += 4
+    const value = this.view.getInt32(this.cursor, this.endian);
+    this.cursor += 4;
     return value
   }
 
   getUint32() {
-    const value = this.view.getUint32(this.i, this.endian)
-    this.i += 4
+    const value = this.view.getUint32(this.cursor, this.endian);
+    this.cursor += 4;
     return value
   }
 
   getFloat32() {
-    const value = this.view.getFloat32(this.i, this.endian)
-    this.i += 4
+    const value = this.view.getFloat32(this.cursor, this.endian);
+    this.cursor += 4;
     return value
   }
 
   getFloat64() {
-    const value = this.view.getFloat64(this.i, this.endian)
-    this.i += 8
+    const value = this.view.getFloat64(this.cursor, this.endian);
+    this.cursor += 8;
     return value
   }
 
@@ -90,13 +90,13 @@ class Sketch {
   load(path) {
     this.loaded = fetch(path)
       .then(res => res.arrayBuffer())
-      .then(this.decode.bind(this))
+      .then(this.decode.bind(this));
   }
 
   decode(buffer) {
 
-    const view = new DataView(buffer, 0)
-    const reader = new Reader(view, true)
+    const view = new DataView(buffer, 0);
+    const reader = new Reader(view, true);
 
     this.header = {
       sentinel:       text(reader.getUint32()),
@@ -104,13 +104,13 @@ class Sketch {
       header_version: reader.getUint16(),
       reserved_1:     reader.getUint32(),
       reserved_2:     reader.getUint32()
-    }
+    };
 
     if(this.header.sentinel != 'tilT') {
       throw "File Reading Error (sentinel mismatch)"
     }
 
-    this.zip = new JSZip()
+    this.zip = new JSZip();
 
     return this.zip.loadAsync(buffer.slice(16))
 
@@ -124,49 +124,49 @@ class Sketch {
 
   data() {
 
-    const _data = {}
+    const _data = {};
 
     return this.loaded
       .then( _ =>
         this.zip.file('data.sketch').async('arraybuffer')
       )
       .then( buffer => {
-        const view = new DataView(buffer, 0)
-        const reader = new Reader(view, true)
+        const view = new DataView(buffer, 0);
+        const reader = new Reader(view, true);
 
-        _data.sentinel = reader.getUint32()
-        _data.version  = reader.getUint32()
-        _data.reserved = reader.getUint32()
+        _data.sentinel = reader.getUint32();
+        _data.version  = reader.getUint32();
+        _data.reserved = reader.getUint32();
 
         // skip any additional information
-        const size = reader.getUint32()
-        reader.skip(size)
+        const size = reader.getUint32();
+        reader.skip(size);
 
-        const stroke_count = reader.getUint32()
+        const stroke_count = reader.getUint32();
 
         _data.strokes = range(stroke_count)
           .map(i => {
-            const _stroke = {}
+            const _stroke = {};
 
-            _stroke.brush_index = reader.getInt32()
+            _stroke.brush_index = reader.getInt32();
             _stroke.brush_color =
-              [reader.getFloat32(), reader.getFloat32(), reader.getFloat32(), reader.getFloat32()]
-            _stroke.brush_size  = reader.getFloat32()
+              [reader.getFloat32(), reader.getFloat32(), reader.getFloat32(), reader.getFloat32()];
+            _stroke.brush_size  = reader.getFloat32();
 
 
-            const stroke_ext = reader.getUint32()
-            const ctrlpt_ext = reader.getUint32()
+            const stroke_ext = reader.getUint32();
+            const ctrlpt_ext = reader.getUint32();
 
             bits(stroke_ext & 0xffff)
-              .forEach(b => { if(b) reader.skip(4) })
+              .forEach(b => { if(b) reader.skip(4); });
 
             bits(stroke_ext & ~0xffff)
-              .forEach(b => { if(b) reader.skip(reader.getUint32()) })
+              .forEach(b => { if(b) reader.skip(reader.getUint32()); });
 
 
-            const num_points = reader.getUint32()
+            const num_points = reader.getUint32();
 
-            const skipCount = bits(ctrlpt_ext).reduce((a, b) => a + b, 0) * 4
+            const skipCount = bits(ctrlpt_ext).reduce((a, b) => a + b, 0) * 4;
 
             _stroke.points = range(num_points)
               .map(j => {
@@ -175,26 +175,26 @@ class Sketch {
                   reader.getFloat32(),
                   reader.getFloat32(),
                   reader.getFloat32()
-                ]
+                ];
 
                 const orientation = [
                   reader.getFloat32(),
                   reader.getFloat32(),
                   reader.getFloat32(),
                   reader.getFloat32()
-                ]
+                ];
 
                 // this is useful information (pressure & timestamp)
-                reader.skip(skipCount)
+                reader.skip(skipCount);
 
                 return { position, orientation }
 
-              })
+              });
 
 
             return _stroke
 
-          })
+          });
 
         return _data
 
@@ -207,25 +207,25 @@ class Renderer {
 
   constructor() {
 
-    const canvas = document.createElement('canvas')
-    const size = Math.min(window.innerHeight,window.innerWidth)
-    const ratio = window.devicePixelRatio || 1
+    const canvas = document.createElement('canvas');
+    const size = Math.min(window.innerHeight,window.innerWidth);
+    const ratio = window.devicePixelRatio || 1;
 
-    canvas.width = canvas.height = size * ratio
-    canvas.style.width = canvas.style.height = size + 'px'
+    canvas.width = canvas.height = size * ratio;
+    canvas.style.width = canvas.style.height = size + 'px';
 
-    document.body.appendChild(canvas)
+    document.body.appendChild(canvas);
 
-    this.ctx = canvas.getContext('2d')
+    this.ctx = canvas.getContext('2d');
 
-    this.ctx.translate(size*ratio/2,size*ratio/2)
-    this.ctx.scale(15,15)
+    this.ctx.translate(size*ratio/2,size*ratio/2);
+    this.ctx.scale(15,15);
 
   }
 
   setData(data) {
     data.strokes.forEach( stroke => {
-      this.ctx.fillStyle = 'rgba(' + stroke.brush_color.map(c => Math.floor(c * 255)).join(', ') + ')'
+      this.ctx.fillStyle = 'rgba(' + stroke.brush_color.map(c => Math.floor(c * 255)).join(', ') + ')';
 
       stroke.points.forEach( point => {
         if(point.position[2] > 0)
@@ -233,10 +233,10 @@ class Renderer {
           point.position[0],
           -point.position[1],
           .1,.1
-        )
-      })
+        );
+      });
 
-    })
+    });
   }
 
 }
